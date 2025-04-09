b21754b80a83b816d1d73a6a9f3121ad9064de01
Hari Kuchipudi <harikuchipudi1@gmail.com>
2025-04-10 00:09:30 +0530

    Ignore db for now

--- src/main/java/com/example/project/AreaCalcApplication.java
+++ src/main/java/com/example/project/AreaCalcApplication.java
@@ -2,8 +2,14 @@ package com.example.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
 public class AreaCalcApplication {
 
 	public static void main(String[] args) {
