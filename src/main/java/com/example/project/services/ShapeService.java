1c05e06a2b5f034db1b3b0b9a0858e8d6539e9f3
Hari Kuchipudi <harikuchipudi1@gmail.com>
2025-04-10 00:35:12 +0530

    config update-3

--- src/main/java/com/example/project/services/ShapeService.java
+++ src/main/java/com/example/project/services/ShapeService.java
@@ -3,6 +3,7 @@ package com.example.project.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import com.example.project.models.Circle;
@@ -19,6 +20,7 @@ import com.example.project.repository.TriangleRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
+@Profile("!docker")
 public class ShapeService {
 	
 	
